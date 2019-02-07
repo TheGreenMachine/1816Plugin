ba9b45acca6c9281913d5b06d07d7ac8a0edab38
Ryan K <kooryan03@gmail.com>
2019-02-07 10:18:24 -0600

    customization

--- src/main/java/CoordinateWidget.java
+++ src/main/java/CoordinateWidget.java
@@ -130,7 +130,6 @@ public class CoordinateWidget extends SimpleAnnotatedWidget {
         }
     }
 
-
     public void addDataToSeries() {
         for (int i = 0; i < 20; i++) {
             if (pointY.isEmpty())
