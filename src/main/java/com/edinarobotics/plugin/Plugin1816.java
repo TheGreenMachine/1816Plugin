c0535075a0fff504bbc7173b478e910f35216f28
Richik SC <sc.richik@gmail.com>
2020-12-07 21:46:42 -0600

    Add Green Machine Shuffleboard theme to plugin

--- src/main/java/com/edinarobotics/plugin/Plugin1816.java
+++ src/main/java/com/edinarobotics/plugin/Plugin1816.java
@@ -7,14 +7,17 @@ import com.google.common.collect.ImmutableList;
 import edu.wpi.first.shuffleboard.api.data.DataType;
 import edu.wpi.first.shuffleboard.api.plugin.Description;
 import edu.wpi.first.shuffleboard.api.plugin.Plugin;
+import edu.wpi.first.shuffleboard.api.theme.Theme;
 import edu.wpi.first.shuffleboard.api.widget.ComponentType;
 import edu.wpi.first.shuffleboard.api.widget.WidgetType;
 
 import java.util.List;
 
 
-@Description(group = "com.edinarobotics", name = "Green Machine Plugin", version = "2.2.1", summary = "Base Plugin for all Green Machine Widgets")
+@Description(group = "com.edinarobotics", name = "Green Machine Plugin", version = "2.3.0", summary = "Base Plugin for all Green Machine Widgets")
 public class Plugin1816 extends Plugin {
+
+    private static final Theme theme = new Theme(Plugin1816.class, "Green Machine", "/com/edinarobotics/plugin/greenmachinetheme.css");
 
     public Plugin1816() {
         super();
@@ -34,5 +37,10 @@ public class Plugin1816 extends Plugin {
         return ImmutableList.of(
             Point2DType.Instance
         );
+    }
+
+    @Override
+    public List<Theme> getThemes() {
+        return ImmutableList.of(theme);
     }
 }
